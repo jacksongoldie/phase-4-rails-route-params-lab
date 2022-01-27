ec08adfb580e824f9d857ed4ff31e7e0c18e559c
root <root@LAPTOP-78CKJOIP.localdomain>
2022-01-27 13:35:45 -0600

    commit tree d6d74d45f6bb192212329c3ff0b8110c3c1d7bc8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
+
+    #byebug
     students = Student.all
-    render json: students
+
+    if request.params.present?
+      filtered = students.filter {|s| s.first_name == params[:first_name] && s.last_name == params[:last_name]}
+      render json: filtered
+    else 
+      render json: students
+    end
+      
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end

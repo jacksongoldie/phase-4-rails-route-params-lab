8a03c20012f453fb24101026e9c4d6ae8600359c
root <root@LAPTOP-78CKJOIP.localdomain>
2022-01-27 13:39:50 -0600

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,20 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+
+    if request.params.present?
+      filtered = students.filter {|s| s.first_name == params[:first_name] && s.last_name == params[:last_name]}
+      #byebug
+      render json: filtered
+    else 
+      render json: students
+    end
+      
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
